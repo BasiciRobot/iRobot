f6aec5e8b6f0f1bd30c9477a3375f12a2cfc4c77
weichen8157 <weichen8157@gmail.com>
2015-06-18 16:50:49 +0800

    detect update

--- test.c
+++ test.c
@@ -1,13 +1,5 @@
 #include <wiringPi.h>
 #include <stdlib.h>
-unsigned int count=0;
-unsigned int currentLeft,currentRight,currentLLeft,currentRRight;
-
-/*dimension
-Width: 16 cm
-Length: 24 cm
-ratio 2:3
-*/
 
 void trigger(int pin)
 {
@@ -105,83 +97,108 @@ void stop(int in1,int in2,int in3,int in4,int in5,int in6,int in7,int in8)
     digitalWrite(in8,LOW);
 }
 
-int main (void)
-{
-  wiringPiSetup () ;
-  pinMode (0, OUTPUT) ;//Ultrasonic 1
-  pinMode (1, INPUT) ;
-  pinMode (2, OUTPUT) ;//Ultrasonic 2
-  pinMode (3, INPUT) ;
-  pinMode (4, OUTPUT) ;//Ultrasonic 3
-  pinMode (5, INPUT) ;
-  pinMode (6, OUTPUT) ;//Ultrasonic 4
-  pinMode (7, INPUT) ;
+struct region{
+	char up=0;
+	char left=0;
+	char right=0;
+	char back=0;
+	char state = 0;	//0=unknown 1=free 2=obstacle;
+};
 
-  pinMode (8,OUTPUT);//Motor 1
-  pinMode (9,OUTPUT);
-  pinMode (10,OUTPUT);//Motor 2
-  pinMode (11,OUTPUT);
-  pinMode (12,OUTPUT);//Motor 3
-  pinMode (13,OUTPUT);
-  pinMode (14,OUTPUT);//Motor 4
-  pinMode (15,OUTPUT);
-  stop(8,9,10,11,12,13,14,15);
+typedef struct car{
+    volatile float x;// car position
+    volatile float y;
 
-  for(;;)
-  {
+    char mode;          // running mode,0:first run
+    char direction; // 0:North 1:East 2:South 3:West
+}CAR;
+
+void detect(){
      unsigned int timeLeft,timeRight,timeLLeft,timeRRight;
-     unsigned int distLeft,distRight,distLLeft,distRRight;
 
      trigger(0);
      timeLeft = pulseIn(1,HIGH,500000);
-     distLeft = distance(timeLeft);
-     if(distLeft >0)
-     {
-         currentLeft=distLeft;
-     }
-
+     currentLeft = distance(timeLeft);
+     
      trigger(2);  
      timeRight = pulseIn(3,HIGH,500000);
-     distRight = distance(timeRight);
-     if(distRRight >0)
-     {
-         currentRight=distRight;
-     }
+     currentRight = distance(timeRight);
 
      trigger(4);
      timeLLeft = pulseIn(5,HIGH,500000);
-     distLLeft = distance(timeLLeft);
-     if(distLLeft >0)
-     {
-         currentLLeft=distLLeft;
-     }
-
+     currentLLeft = distance(timeLLeft);
+     
      trigger(6);  
      timeRRight = pulseIn(7,HIGH,500000);
-     distRRight = distance(timeRRight);
-     if(distRRight >0)
-     {
-         currentRRight=distRRight;
-     }
-
-     if(currentLeft<=240 && currentRight <=240 && currentRRight <=240 && currentRRight <=240)
-     {
-         backward(8,9,10,11,12,13,14,15);
-     }
-     else if (currentLeft>240 && currentRight >240)
-     {
-         forward(8,9,10,11,12,13,14,15);
-     }
-     else if (currentLeft<=240 && currentRRight >240)
-     {
-         turnRight(8,9,10,11,12,13,14,15);
-     }
-     else if (currentRight<=240 && currentLLeft >240)
-     {
-         turnLeft(8,9,10,11,12,13,14,15);
-     }
-     printf("distLeft: %d distRight: %d count zero: %d\n",distLeft,distRight,count);
-     delay (1000);
-  }
-  return 0 ;
+     currentRRight = distance(timeRRight);
+     
 }
+
+void init(){
+	car.x=1;
+    	car.y=1;
+	car.direction = 0;
+	map[0][0].state = free;
+	int i,j;
+    	for(i=0;i<X;i++){
+       		 for(j=0;j<Y;j++){
+            		if(j==0||i==0||i==X||j==Y)
+                		map[i][j] = 2; 
+        	}
+   	 }
+}
+
+void update_map(CAR car){
+	
+	//距離換算成單位
+	unsigned int Leftunit = currentLeft/unit;
+	unsigned int Rightunit = currentRight/unit;
+	unsigned int LLeftunit = currentLLeft/unit;
+	unsigned int RRightunit = currentRRight/unit;
+	unsigned int Frontunit;
+
+	//判斷前方左or右較近，取近的
+	if(rightunit>leftunit)		
+		Frontunit = Leftunit;
+	else 
+		Frontunit = Rightunit;
+	
+	//根據單位，決定前後左右state
+	if(Leftunit==0 && currentLLeft!=0)	//currentLLeft=0 超過可測距離範圍，表示下個單位無障礙物
+		map[car.x][car.y].left = 2;
+	else
+		map[car.x][car.y].left = 1;
+	
+	if(Rightunit==0 && currentRRight!=0)
+		map[car.x][car.y].right = 2;
+	else
+		map[car.x][car.y].right = 1;
+	
+	if(Frontunit==0 && (currentLeft!=0 || currentRight!=0))
+		map[car.x][car.y].up = 2;
+	else
+		map[car.x][car.y].up = 1;
+	
+	if(car.direction==0)
+		map[car.x][car.y].back = map[car.x][car.y-1].state;
+	else if(car.direction==2)
+		map[car.x][car.y].back = map[car.x][car.y+1].state;
+	
+}
+
+//global變數
+unsigned int unit 1 ;			//單位大小
+volatile unsigned int currentLeft,currentRight,currentLLeft,currentRRight;
+
+CAR car;
+
+int X 256;
+int Y 128;	 
+struct region map[X][Y];
+
+
+
+int main(void){
+	
+}
+
