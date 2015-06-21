47f3d9e85f2ba14a2cf1ae0fa78b98ce0a53c66f
weichen8157 <weichen8157@gmail.com>
2015-06-21 18:24:26 +0800

    18:24

--- updatemap.h
+++ updatemap.h
@@ -1,140 +1,115 @@
 void updatemap(){
-
-    if(car.turn!=NONE){}
-    else{
-        if (currentLFront > DISTANCE && currentRFront > DISTANCE)
-        {
-            car.map[car.x][car.y] |= UPFREE;   
-        }
-        else if(currentLFront <= DISTANCE && currentRFront <= DISTANCE)
-        {
-            if (currentLeft > DISTANCE && currentRight > DISTANCE)
-            {
-                    switch (car.turn)
-                    {
-                        case RIGHT:
-                        turnLeft();                    
-                        break;
-                        case LEFT:
-                        turnRight();
-                        break;
-                        default:
-                        turnRight();
-                    }
-            }
-            else if(currentLeft <= DISTANCE && currentRight <= DISTANCE)
-            {
-                moveBackward();
-            }
-            else if (currentLeft <= DISTANCE && currentRight > DISTANCE)
-            {
-                turnRight();
-            }
-            else
-            {
-                turnLeft();
-            }
-        }
-        else if (currentLFront <= DISTANCE && currentRFront > DISTANCE)
-        {
-            if (currentLeft > DISTANCE && currentRight > DISTANCE)
-            {
-                    switch (car.turn)
-                    {
-                        case RIGHT:
-                        turnLeft();
-                        break;
-                        case LEFT:
-                        turnRight();
-                        break;
-                        default:
-                        turnRight();
-                    }
-            }
-            else if(currentLeft <= DISTANCE && currentRight <= DISTANCE)
-            {
-                moveBackward();   
-            }
-            else if (currentLeft <= DISTANCE && currentRight > DISTANCE)
-            {
-                turnRight();
-            }
-            else
-            {
-                turnLeft();
-            }
-        }
-        else
-        {
-            if (currentLeft > DISTANCE && currentRight > DISTANCE)
-            {
-                    switch (car.turn)
-                    {
-                        case RIGHT:
-                        turnLeft();
-                        break;
-                        case LEFT:
-                        turnRight();
-                        break;
-                        default:
-                        turnRight();
-                    }
-            }
-            else if(currentLeft <= DISTANCE && currentRight <= DISTANCE)
-            {
-                moveBackward();
-            }
-            else if (currentLeft <= DISTANCE && currentRight > DISTANCE)
-            {
-                turnRight();
-            }
-            else
-            {
-                turnLeft();
-            }
-        }
-    }
-
-
-
-
-}
-
-void updatemap(){
 	
     unsigned int unit = 1;
-	//¶ZÂ÷´«ºâ¦¨³æ¦ì
+	
 	unsigned int Leftunit = currentLeft/unit;
 	unsigned int Rightunit = currentRight/unit;
 	unsigned int LFrontunit = currentLFront/unit;
 	unsigned int RFrontunit = currentRFront/unit;
 	unsigned int Frontunit;
 
-	if(RFrontunit > LFrontunit)		
+	if(currentRFront > currentLFront)		
 		Frontunit = LFrontunit;
 	else 
 		Frontunit = RFrontunit;
+    
+        
+
+if(car.direction == UP){	
 	
-	//®Ú¾Ú³æ¦ì¡A¨M©w«e«á¥ª¥kstate
-	if(LFrontunit==0 && currentLeft!=0)	//currentLLeft=0 ¶W¹L¥i´ú¶ZÂ÷½d³ò¡Aªí¥Ü¤U­Ó³æ¦ìµL»ÙÃªª«
-		map[car.x][car.y].left = 2;
+	if(Leftunit==0 && currentLeft!=0)
+		car.map[car.x][car.y] &= LEFTWALL;
 	else
-		map[car.x][car.y].left = 1;
+		car.map[car.x][car.y] |= LERTFREE;
 	
-	if(RFrontunit==0 && currentRight!=0)
-		map[car.x][car.y].right = 2;
+	if(Rightunit==0 && currentRight!=0)
+		car.map[car.x][car.y] &= RIGHTWALL;
 	else
-		map[car.x][car.y].right = 1;
+		car.map[car.x][car.y] |= RIGHTFREE;
 	
 	if(Frontunit==0 && (currentLFront!=0 || currentRFront!=0))
-		map[car.x][car.y].up = 2;
+		car.map[car.x][car.y] &= UPWALL;
 	else
-		map[car.x][car.y].up = 1;
+		car.map[car.x][car.y] |= UPFREE;I
+
+    if(car.map[car.x][car.y-1] &= FREE != 0)	
+	    car.map[car.x][car.y] |= DWONFREE;
+    else
+        car.map[car.x][car.y] &= DOWNWALL;
+}
+
+if(car.direction == DOWN){	
 	
-	if(car.direction==UP)
-		map[car.x][car.y].back = map[car.x][car.y-1].state;
-	else if(car.direction==DOWN)
-		map[car.x][car.y].back = map[car.x][car.y+1].state;
+	if(Leftunit==0 && currentLeft!=0)
+		car.map[car.x][car.y] &= RIGHTWALL;
+	else
+		car.map[car.x][car.y] |= RIGHTFREE;
 	
+	if(Rightunit==0 && currentRight!=0)
+		car.map[car.x][car.y] &= LEFTWALL;
+	else
+		car.map[car.x][car.y] |= LRFTFREE;
+	
+	if(Frontunit==0 && (currentLFront!=0 || currentRFront!=0))
+		car.map[car.x][car.y] &= DOWNWALL;
+	else
+		car.map[car.x][car.y] |= DOWNFREE;
+	
+    if(car.map[car.x][car.y+1] &= FREE != 0)	
+	    car.map[car.x][car.y] |= UPFREE;
+    else
+        car.map[car.x][car.y] &= UPWALL;
+    
+}
+
+if(car.direction == LEFT){	
+	
+	if(Leftunit==0 && currentLeft!=0)
+		car.map[car.x][car.y] &= DOWNWALL;
+	else
+		car.map[car.x][car.y] |= DOWNFREE;
+	
+	if(Rightunit==0 && currentRight!=0)
+		car.map[car.x][car.y] &= UPWALL;
+	else
+		car.map[car.x][car.y] |= UPFREE;
+	
+	if(Frontunit==0 && (currentLFront!=0 || currentRFront!=0))
+		car.map[car.x][car.y] &= LEFTWALL;
+	else
+		car.map[car.x][car.y] |= LEFTFREE;
+	
+    if(car.map[car.x+1][car.y] &= FREE != 0)	
+	    car.map[car.x][car.y] |= RIGHTFREE;
+    else
+        car.map[car.x][car.y] &= RIGHTWALL;
+	
+}
+
+if(car.direction == RIGHT){	
+	
+	if(Leftunit==0 && currentLeft!=0)
+		car.map[car.x][car.y] &= UPWALL;
+	else
+		car.map[car.x][car.y] |= UPFREE;
+	
+	if(Rightunit==0 && currentRight!=0)
+		car.map[car.x][car.y] &= DOWNWALL;
+	else
+		car.map[car.x][car.y] |= DOWNFREE;
+	
+	if(Frontunit==0 && (currentLFront!=0 || currentRFront!=0))
+		car.map[car.x][car.y] &= RIGHTWALL;
+	else
+		car.map[car.x][car.y] |= RIGHTFREE;
+	
+    if(car.map[car.x-1][car.y] &= FREE != 0)	
+	    car.map[car.x][car.y] |= LEFTFREE;
+    else
+        car.map[car.x][car.y] &= LEFTWALL;
+	
+}
+
+
 }
 
