baca554cf4fba97702a74469d91e6a993e57ea34
weichen8157 <weichen8157@gmail.com>
2015-06-22 14:38:54 +0800

    updatemap

--- updatemap.h
+++ updatemap.h
@@ -18,25 +18,26 @@ if(car.direction == UP){
 	
 	if(Leftunit==0 && currentLeft!=0)
 		car.map[car.x][car.y] &= LEFTWALL;
-                car.map[car.x-1][car.y] &= NOFREE; 
+        car.map[car.x-1][car.y] &= NOFREE; 
 	else
 		car.map[car.x][car.y] |= LERTFREE;
-                car.map[car.x-1][car.y] |= FREE;
+        car.map[car.x-1][car.y] |= FREE;
 	
 	if(Rightunit==0 && currentRight!=0)
 		car.map[car.x][car.y] &= RIGHTWALL;
-                car.map[car.x+1][car.y] &= NOFREE
+        car.map[car.x+1][car.y] &= NOFREE
 	else
 		car.map[car.x][car.y] |= RIGHTFREE;
-                car.map[car.x+1][car.y] |= FREE; 
+        car.map[car.x+1][car.y] |= FREE; 
 	
 	if(Frontunit==0 && (currentLFront!=0 || currentRFront!=0))
 		car.map[car.x][car.y] &= UPWALL;
-                car.map[car.x][car.y+1] &= NOFREE;
+        car.map[car.x][car.y+1] &= NOFREE;
 	else
-		car.map[car.x][car.y] |= UPFREE;I
+		car.map[car.x][car.y] |= UPFREE;
+        car.map[car.x][car.y+1] |= FREE;
 
-    if(car.map[car.x][car.y-1] &= FREE != 0)	
+    if((car.map[car.x][car.y-1] &= FREE) != 0)	
 	    car.map[car.x][car.y] |= DWONFREE;
     else
         car.map[car.x][car.y] &= DOWNWALL;
@@ -46,20 +47,26 @@ if(car.direction == DOWN){
 	
 	if(Leftunit==0 && currentLeft!=0)
 		car.map[car.x][car.y] &= RIGHTWALL;
+        car.map[car.x+1][car.y] &= NOFREE;
 	else
 		car.map[car.x][car.y] |= RIGHTFREE;
-	
+        car.map[car.x+1][car.y] |= FREE;
+
 	if(Rightunit==0 && currentRight!=0)
 		car.map[car.x][car.y] &= LEFTWALL;
+		car.map[car.x-1][car.y] &= NOFREE;
 	else
 		car.map[car.x][car.y] |= LRFTFREE;
+		car.map[car.x-1][car.y] |= FREE;
 	
 	if(Frontunit==0 && (currentLFront!=0 || currentRFront!=0))
 		car.map[car.x][car.y] &= DOWNWALL;
+		car.map[car.x][car.y-1] &= NOFREE;
 	else
 		car.map[car.x][car.y] |= DOWNFREE;
-	
-    if(car.map[car.x][car.y+1] &= FREE != 0)	
+        car.map[car.x][car.y-1] |= FREE;
+
+    if((car.map[car.x][car.y+1] &= FREE) != 0)	
 	    car.map[car.x][car.y] |= UPFREE;
     else
         car.map[car.x][car.y] &= UPWALL;
@@ -70,20 +77,27 @@ if(car.direction == LEFT){
 	
 	if(Leftunit==0 && currentLeft!=0)
 		car.map[car.x][car.y] &= DOWNWALL;
+        car.map[car.x][car.y-1] &= NOFREE;
 	else
 		car.map[car.x][car.y] |= DOWNFREE;
-	
+        car.map[car.x][car.y-1] |= FREE;
+
 	if(Rightunit==0 && currentRight!=0)
 		car.map[car.x][car.y] &= UPWALL;
+        car.map[car.x][car.y+1] &= NOFREE;
 	else
 		car.map[car.x][car.y] |= UPFREE;
-	
+        car.map[car.x][car.y+1] |= FREE;
+ 
 	if(Frontunit==0 && (currentLFront!=0 || currentRFront!=0))
 		car.map[car.x][car.y] &= LEFTWALL;
+        car.map[car.x-1][car.y] &= NOFREE;
 	else
 		car.map[car.x][car.y] |= LEFTFREE;
+        car.map[car.x-1][car.y] |= FREE;
+ 
 	
-    if(car.map[car.x+1][car.y] &= FREE != 0)	
+    if((car.map[car.x+1][car.y] &= FREE) != 0)	
 	    car.map[car.x][car.y] |= RIGHTFREE;
     else
         car.map[car.x][car.y] &= RIGHTWALL;
@@ -94,20 +108,26 @@ if(car.direction == RIGHT){
 	
 	if(Leftunit==0 && currentLeft!=0)
 		car.map[car.x][car.y] &= UPWALL;
+		car.map[car.x][car.y+1] &= NOFREE;
 	else
 		car.map[car.x][car.y] |= UPFREE;
+		car.map[car.x][car.y+1] |= FREE;
 	
 	if(Rightunit==0 && currentRight!=0)
-		car.map[car.x][car.y] &= DOWNWALL;
+		car.map[car.x][car.y] &= DOWNWALL; 
+		car.map[car.x][car.y-1] &= NOFREE;
 	else
-		car.map[car.x][car.y] |= DOWNFREE;
+		car.map[car.x][car.y] |= DOWNFREE; 
+		car.map[car.x][car.y-1] |= FREE;
 	
 	if(Frontunit==0 && (currentLFront!=0 || currentRFront!=0))
 		car.map[car.x][car.y] &= RIGHTWALL;
+		car.map[car.x+1][car.y] &= NOFREE;
 	else
 		car.map[car.x][car.y] |= RIGHTFREE;
-	
-    if(car.map[car.x-1][car.y] &= FREE != 0)	
+		car.map[car.x+1][car.y] |= FREE;
+        
+    if((car.map[car.x-1][car.y] &= FREE) != 0)	
 	    car.map[car.x][car.y] |= LEFTFREE;
     else
         car.map[car.x][car.y] &= LEFTWALL;
